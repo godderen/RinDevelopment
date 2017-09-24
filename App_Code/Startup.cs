2770080fbe1f049360c5dca2c3341a52f7112f31
godderen <godderen67@gmail.com>
2017-09-24 19:03:46 +0900

    2017/09/24 测试

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RinDevelopment.Startup))]
+namespace RinDevelopment
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
